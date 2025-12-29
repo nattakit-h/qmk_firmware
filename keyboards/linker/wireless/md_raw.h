5a423b08df715f9e6244c62698ff94f5cfd4dcbe
Nattakit Hosapsin <nattakit@hosapsin.com>
2025-12-29 17:53:09 +0700

    Add VIA keymap

--- keyboards/linker/wireless/md_raw.h
+++ keyboards/linker/wireless/md_raw.h
@@ -7,4 +7,4 @@
 #define COMBINE(A, B) A##B
 #define raw_hid_send(a, b) RENAME_WITH_LINE(_temp_rhs_, __LINE__)(a, b)
 #define _temp_rhs_29 replaced_hid_send  // raw_hid.h
-#define _temp_rhs_461 replaced_hid_send // via.c
+#define _temp_rhs_481 replaced_hid_send // via.c
