d969bf5517c3fdc0b0a0a38383113a2a4d6a5447
Nattakit Hosapsin <nattakit@hosapsin.com>
2026-01-06 12:24:48 +0700

    Merge wls files to the main source file

--- keyboards/monsgeek/m1_v5/m1_v5_us/m1_v5_us.c
+++ keyboards/monsgeek/m1_v5/m1_v5_us/m1_v5_us.c
@@ -4,11 +4,28 @@
 #include QMK_KEYBOARD_H
 #include "data.h"
 #include "indicators.h"
-#include "wls/wls.h"
 
 #include "wireless.h"
 #include "usb_main.h"
 #include "lowpower.h"
+
+#define HS_MODEIO_DETECTION_TIME          50
+#define HS_LBACK_TIMEOUT                  (30 * 1000)
+#define HS_SLEEP_TIMEOUT                  (1 * 60000)
+
+enum modeio_mode {
+    hs_none = 0,
+    hs_usb,
+    hs_bt,
+    hs_2g4,
+    hs_wireless
+};
+
+bool hs_rgb_blink_hook(void);
+bool hs_mode_scan(bool update, uint8_t mode);
+bool hs_modeio_detection(bool update, uint8_t *mode);
+void hs_rgb_blink_set_timer(uint32_t time);
+
 
 /*****************************************************************************/
 /*                                  Data                                     */
@@ -408,3 +425,79 @@ void palcallback_cb(uint8_t line) {
         } break;
     }
 }
+
+/*****************************************************************************/
+
+bool hs_modeio_detection(bool update, uint8_t *mode) {
+    static uint32_t scan_timer = 0x00;
+
+    if ((update != true) && (timer_elapsed32(scan_timer) <= (HS_MODEIO_DETECTION_TIME))) {
+        return false;
+    }
+    scan_timer = timer_read32();
+    *mode = hs_none;
+    return false;
+}
+
+static uint32_t hs_linker_rgb_timer = 0x00;
+
+bool hs_mode_scan(bool update, uint8_t mode) {
+
+    if (hs_modeio_detection(update, &mode)) {
+
+        return true;
+    }
+    hs_rgb_blink_hook();
+    return false;
+}
+
+void hs_rgb_blink_set_timer(uint32_t time) {
+    hs_linker_rgb_timer = time;
+}
+
+uint32_t hs_rgb_blink_get_timer(void) {
+    return hs_linker_rgb_timer;
+}
+
+bool hs_rgb_blink_hook() {
+    static uint8_t last_status;
+
+    if (last_status != *md_getp_state()) {
+        last_status = *md_getp_state();
+        hs_rgb_blink_set_timer(0x00);
+    }
+
+    switch (*md_getp_state()) {
+        case MD_STATE_NONE: {
+            hs_rgb_blink_set_timer(0x00);
+        } break;
+
+        case MD_STATE_DISCONNECTED:
+            if (hs_rgb_blink_get_timer() == 0x00) {
+                hs_rgb_blink_set_timer(timer_read32());
+                extern void wireless_devs_change_kb(uint8_t old_devs, uint8_t new_devs, bool reset);
+                wireless_devs_change_kb(wireless_get_current_devs(), wireless_get_current_devs(), false);
+            } else {
+                if (timer_elapsed32(hs_rgb_blink_get_timer()) >= HS_LBACK_TIMEOUT) {
+                    hs_rgb_blink_set_timer(timer_read32());
+                    md_send_devctrl(MD_SND_CMD_DEVCTRL_USB);
+                    wait_ms(200);
+                    lpwr_set_timeout_manual(true);
+                }
+            }
+        case MD_STATE_CONNECTED:
+            if (hs_rgb_blink_get_timer() == 0x00) {
+                hs_rgb_blink_set_timer(timer_read32());
+            } else {
+                if (timer_elapsed32(hs_rgb_blink_get_timer()) >= HS_SLEEP_TIMEOUT) {
+                    hs_rgb_blink_set_timer(timer_read32());
+                    lpwr_set_timeout_manual(true);
+                }
+            }
+        default:
+            break;
+    }
+    return true;
+}
+
+
